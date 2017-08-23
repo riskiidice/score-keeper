f33a2c2f65955b8218f73af5aa75a1399878165f
Tikumporn Wankvar <thesecondstageshow@gmail.com>
2017-08-24 00:26:23 +0700

    Adding JSX

--- client/main.js
+++ client/main.js
@@ -1,4 +1,10 @@
-import someDefault from './../imports/utils'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
 
-console.log('Log from /client/main.js');
-console.log(someDefault);
+let name = "Tikumporn"
+let jsx = <div><p>Hello { name }</p><p>This is second p</p></div>
+
+Meteor.startup(function(){
+  ReactDOM.render(jsx, document.getElementById('app'));
+});
