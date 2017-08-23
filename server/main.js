6d349446e0e9250607ea4dbd9337f72c5ce0f9b9
Tikumporn Wankvar <tikumporn.w@pea.co.th>
2017-08-23 13:04:05 +0700

    Adding basic import/export

--- server/main.js
+++ server/main.js
@@ -1,4 +1,6 @@
+import { add } from './../imports/math'
 import { greetUser } from './../imports/utils'
 
 console.log('Log from /server/main.js');
 console.log(greetUser());
+console.log(add(5,3));
